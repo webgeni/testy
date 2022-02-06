77ec6bd0f696e95cd6d8220c27155168502d1677
webgeni <98998119+webgeni@users.noreply.github.com>
2022-02-06 23:57:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ export const StyledButtonConnect = styled.button`
  background: #d5f365;
  border: none;
  font-family: inherit;
+ font-size: 12px;
  text-align: center;
  cursor: pointer;
  transition: 0.4s;
