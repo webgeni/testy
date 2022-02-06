842e31c317d56c64acf3cc83a0dc7245e4100707
webgeni <98998119+webgeni@users.noreply.github.com>
2022-02-06 23:40:38 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,6 +46,42 @@ export const StyledButton = styled.button`
   }
 `;
 
+export const StyledButtonConnect = styled.button`
+ background: linear-gradient(-45deg, #3f00b5, #9f69fe, #27c8b7, #3f00b5);
+ background-size: 800% 400%;
+ padding: 1em 2em;
+ display: inline-block;
+ border: none;
+ border-radius: 10px;
+ font-size: 17px;
+ font-weight: 700;
+ color: white;
+ transition: all .5s ease-in-out;
+ animation: gradient 10s infinite cubic-bezier(.62, .28, .23, .99) both;
+:hover {
+ animation: gradient 3s infinite;
+ transform: scale(1.05);
+}
+
+:active {
+ animation: gradient 3s infinite;
+ transform: scale(0.8);
+}
+
+@keyframes gradient {
+ 0% {
+  background-position: 0% 50%;
+ }
+
+ 50% {
+  background-position: 100% 50%;
+ }
+
+ 100% {
+  background-position: 0% 50%;
+  }
+`;
+
 
 export const StyledRoundButton = styled.button`
   padding: 10px;
@@ -333,7 +369,7 @@ function App() {
                       Connect to the {CONFIG.NETWORK.NAME} network
                     </s.TextDescription>
                     <s.SpacerSmall />
-                    <StyledButton
+                    <StyledButtonConnect
                       onClick={(e) => {
                         e.preventDefault();
                         dispatch(connect());
@@ -341,7 +377,7 @@ function App() {
                       }}
                     >
                       CONNECT WALLET
-                    </StyledButton>
+                    </StyledButtonConnect>
                     {blockchain.errorMsg !== "" ? (
                       <>
                         <s.SpacerSmall />
