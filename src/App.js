16f50833e955a33dbb79043004006de74c472cf4
webgeni <98998119+webgeni@users.noreply.github.com>
2022-02-06 20:22:13 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -243,6 +243,7 @@ function App() {
                 textAlign: "center",
                 fontSize: 50,
                 fontWeight: "bold",
+                font-family: "lato", sans-serif,
                 color: "var(--accent-text)",
               }}
             >
