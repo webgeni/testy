d25807f08917fef857ce61b48f75d9e3dd293719
webgeni <98998119+webgeni@users.noreply.github.com>
2022-02-07 01:04:22 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -250,11 +250,11 @@ function App() {
           <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         </a>
         <s.SpacerSmall />
-        
+         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
        <s.Container flex={1} jc={"center"} ai={"center"}>
            
           </s.Container>
-          <s.SpacerLarge />
+          
           <s.Container
             flex={2}
             jc={"center"}
