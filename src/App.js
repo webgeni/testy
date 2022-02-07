e9feb58e24d9542aa80d5e24fd68f9668a8514d2
webgeni <98998119+webgeni@users.noreply.github.com>
2022-02-07 14:55:15 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -250,7 +250,7 @@ function App() {
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
         <a href={CONFIG.MARKETPLACE_LINK}>
-          <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+          <StyledLogo alt={"logo"} src={"/config/images/logo.svg"} />
         </a>
         <s.SpacerSmall />
          <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
