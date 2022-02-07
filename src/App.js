7e667b8f15f367f2566f30d02cc4debd3a760663
webgeni <98998119+webgeni@users.noreply.github.com>
2022-02-07 03:05:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
-  const [feedback, setFeedback] = useState(`Click Purchase to mint your NFT. ${CONFIG.MAX_SUPPLY}`);
+  const [feedback, setFeedback] = useState(`Click Purchase to mint your NFT.`);
   const [mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
